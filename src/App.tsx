2f4373cdae7d1976df54b6691bbce5ae1597fc27
Wishva Kalhara <vishvakalhara@gmail.com>
2024-12-27 09:17:35 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { Label } from "./components/ui/label";
+import { Input } from "./components/ui/input";
+
+function App() {
+    const [email, setEmail] = useState("");
+
+    const submitForm = () => {
+        // Validation
+        //
+    };
+
+    return (
+        <section className="pt-32 px-10 relative">
+            <div className="max-w-md w-full bg-white/5 mx-auto rounded-lg light-shadow p-6 sm:p-10">
+                <h3 className="text-normal md:text-xl  font-semibold leading-relaxed text-center">
+                    Get Early Access
+                </h3>
+                <p className="text-sm/3 mt-2 text-white/70 mx-auto leading-relaxed text-center">
+                    Early access members get exclusive updates and offers.
+                    Don&apos;t miss out!
+                </p>
+                <div className="mt-10 ">
+                    <div className="flex flex-col gap-1">
+                        <Label className="text-xs text-white/70">Email *</Label>
+                        <Input
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                        />
+                    </div>
+                    <Button
+                        onClick={() => {
+                            submitForm();
+                        }}
+                        className="w-full mt-6 py-5 bg-[#C6FCA6]"
+                    >
+                        Join Waitlist
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default App;
